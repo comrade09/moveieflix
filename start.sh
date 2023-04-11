e048b1bc49671262bba50f74f0454a085bbf53e0
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-04-11 19:19:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/comrade09/LazyPrincess /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone https://github.com/comrade09/SamanthaMoviesRobot /SamanthaMoviesRobot
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting SamanthaMoviesRobot...."
 python3 bot.py
